5cfd34fe3cdc961e973ddfa401098264c360fdc5
Edgardo Rodriguez <edgardo@debianLaptop.homenet.io>
2015-08-02 21:01:13 -0600

    Fixed problem with the struct declaration... working binary was created...

--- git_test3.c
+++ git_test3.c
@@ -5,15 +5,16 @@ struct user {
   const char * last_name;
   const char sex;
   int age;
-  bool married;
+  int married;
+  float paycheck;
 };
 
-void print_message ( struct message user_data ) {
-  printf ( " User name is: %s %s, sex identifier is set to %c.", user_data.first_name, user_data.last_name, user_data.sex );
+void print_message ( struct user user_data ) {
+  printf ( "User name is: %s %s, sex identifier is set to %c.\n", user_data.first_name, user_data.last_name, user_data.sex );
 }
 
 int main ( void ) {
-  struct user tina = { "Cristhian", "Rodriguez", 'm', 13, false };
+  struct user tina = { "Cristhian", "Rodriguez", 'm', 13, 1, 10000.99 };
   print_message( tina );
   puts ( "This is a test C program that is been version controlled by Git and GitHub..." );
   return 0;

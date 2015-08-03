faa63f373eca511d221d22a00a7080f86670b2f0
Edgardo Rodriguez <edgardo@debianLaptop.homenet.io>
2015-08-02 20:46:05 -0600

    New custom datatype created 'struct user', added a function that uses as argument this new datatype

--- git_test3.c
+++ git_test3.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+struct user {
+  const char * first_name;
+  const char * last_name;
+  const char sex;
+  int age;
+  bool married;
+};
+
+void print_message ( struct message user_data ) {
+  printf ( " User name is: %s %s, sex identifier is set to %c.", user_data.first_name, user_data.last_name, user_data.sex );
+}
+
 int main ( void ) {
+  struct user tina = { "Cristhian", "Rodriguez", 'm', 13, false };
+  print_message( tina );
   puts ( "This is a test C program that is been version controlled by Git and GitHub..." );
   return 0;
 }
